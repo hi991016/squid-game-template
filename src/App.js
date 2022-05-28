a2414c44823415fb323c93988fecad0b0ea77d15
hi991016 <miureus@gmail.com>
2022-05-28 14:35:20 +0700

    done squid-game-template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+// Components
+import Home from "./components/Home";
+import Info from "./components/Info";
+import Episodes from "./components/Episodes";
+import Videos from "./components/Videos";
+import Details from "./components/Details";
+import Footer from "./components/Footer";
+
+// Scroll
+import scrollreveal from "scrollreveal";
+
+const App = () => {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 2000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        #home,
+        #info,
+        #episodes,
+        #details,
+        #videos,
+        footer
+    `,
+      {
+        opacity: 0,
+        interval: 500,
+      }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <Info />
+      <Episodes />
+      <Videos />
+      <Details />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import styled from "styled-components";
+
+// Utils
+import { fontLightColor } from "../utils/colors";
+
+const Footer = () => {
+  return (
+    <FooterContainer className="defaultMargin">
+      <ul>
+        <li>
+          <a href="#">FAQ</a>
+        </li>
+        <li>
+          <a href="#">Help Centre</a>
+        </li>
+        <li>
+          <a href="#">Account</a>
+        </li>
+        <li>
+          <a href="#">Media Centre</a>
+        </li>
+        <li>
+          <a href="#">Investor Relations</a>
+        </li>
+        <li>
+          <a href="#">Jobs</a>
+        </li>
+        <li>
+          <a href="#">Ways to Watch</a>
+        </li>
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+        <li>
+          <a href="#">Privacy</a>
+        </li>
+        <li>
+          <a href="#">Cookie Preferences</a>
+        </li>
+        <li>
+          <a href="#">Corporate Information</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+        <li>
+          <a href="#">Speed Test</a>
+        </li>
+        <li>
+          <a href="#">Legal Notices</a>
+        </li>
+        <li>
+          <a href="#">Only on Netflix</a>
+        </li>
+      </ul>
+    </FooterContainer>
+  );
+};
+
+// Styled
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: center;
+  border-top: 2px solid ${fontLightColor};
+  margin: 0 5rem;
+  padding-top: 5rem;
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    column-gap: 18rem;
+    row-gap: 2rem;
+    list-style-type: none;
+
+    li {
+      a {
+        text-transform: uppercase;
+        color: ${fontLightColor};
+        text-decoration: none;
+        transition: 0.3s ease-in-out;
+
+        &:hover {
+          color: white;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin: 0 5vw;
+
+    ul {
+      grid-template-columns: repeat(2, 1fr);
+      column-gap: 1rem;
+    }
+  }
+`;
+
+export default Footer;
